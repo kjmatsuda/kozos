7c85ff8e24437713fd499cef0fde99d7cdd1a92d
Koji Matsuda <kjmatsuda1985@gmail.com>
2020-05-23 16:49:06 +0900

    DDR、DRの設定処理をきれいにする

--- led1/os/command.c
+++ led1/os/command.c
@@ -36,13 +36,9 @@ int command_main(int argc, char *argv[])
 	send_use(SERIAL_DEFAULT_DEVICE);
 	// TODO IOポート初期化は別の場所で実行した方がよい
 	// IOポートを初期化
-	uint8 syscr_value = ioport_init();
-	puts("syscr value: ");
-	putxval((unsigned long)syscr_value, 0);
-	puts("\n");
-	ioport_set_data_direction(1, 0xff);
-//	ioport_set_data_direction(2, 0xff);
-//	ioport_set_data_direction(3, 0xff);
+	ioport_init();
+	ioport_set_data_direction(1, 0x81);
+	ioport_set_data_direction(2, 0x81);
 
 	while (1) {
 		send_write("command> "); /* プロンプト表示 */
@@ -55,14 +51,34 @@ int command_main(int argc, char *argv[])
 			send_write(p + 4); /* echoに続く文字列を出力する */
 			send_write("\n");
 		}
-		else if (!strncmp(p, "ledhi", 5)) { /* ledhiコマンド */
-			ioport_set_data(1, 0xff);
-			send_write("ledhi\n");
+		else if (!strncmp(p, "ledhi1_0", 8)) {
+			ioport_set_data(1, 0x01);
+			send_write("ledhi1_0\n");
 			send_write("\n");
 		}
-		else if (!strncmp(p, "ledlo", 5)) { /* ledloコマンド */
+		else if (!strncmp(p, "ledhi1_7", 8)) {
+			ioport_set_data(1, 0x80);
+			send_write("ledhi1_7\n");
+			send_write("\n");
+		}
+		else if (!strncmp(p, "ledhi2_0", 8)) {
+			ioport_set_data(2, 0x01);
+			send_write("ledhi2_0\n");
+			send_write("\n");
+		}
+		else if (!strncmp(p, "ledhi2_7", 8)) {
+			ioport_set_data(2, 0x80);
+			send_write("ledhi2_7\n");
+			send_write("\n");
+		}
+		else if (!strncmp(p, "ledlo1", 6)) {
 			ioport_set_data(1, 0);
-			send_write("ledlo\n");
+			send_write("ledlo1\n");
+			send_write("\n");
+		}
+		else if (!strncmp(p, "ledlo2", 6)) {
+			ioport_set_data(2, 0);
+			send_write("ledlo2\n");
 			send_write("\n");
 		}
 		else {
--- led1/os/ioport.c
+++ led1/os/ioport.c
@@ -1,43 +1,27 @@
 #include "defines.h"
 #include "ioport.h"
 
-// TODO 実際にはポートは11個あるが、ひとまず5個について実装する
-#define IOPORT_NUM 5
+#define IOPORT_NUM 11
 
-#define IOPORT1_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee000)
-#define IOPORT2_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee001)
-#define IOPORT3_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee002)
-#define IOPORT4_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee003)
-#define IOPORT5_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee004)
-#define IOPORT6_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee005)
-#define IOPORT8_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee007)
-#define IOPORT9_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee008)
-#define IOPORTA_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee009)
-#define IOPORTB_DATADIR_REG_ADDR		(*(volatile uint8*)0xfee00A)
+#define DATADIR_REGADDR		((struct data_dir_regs *)0xfee000)
 
-#define IOPORT1_DATA_REG_ADDR		(*(volatile uint8*)0xffffd0)
-#define IOPORT2_DATA_REG_ADDR		(*(volatile uint8*)0xffffd1)
-#define IOPORT3_DATA_REG_ADDR		(*(volatile uint8*)0xffffd2)
-#define IOPORT4_DATA_REG_ADDR		(*(volatile uint8*)0xffffd3)
-#define IOPORT5_DATA_REG_ADDR		(*(volatile uint8*)0xffffd4)
-#define IOPORT6_DATA_REG_ADDR		(*(volatile uint8*)0xffffd5)
-#define IOPORT7_DATA_REG_ADDR		(*(volatile uint8*)0xffffd6)
-#define IOPORT8_DATA_REG_ADDR		(*(volatile uint8*)0xffffd7)
-#define IOPORT9_DATA_REG_ADDR		(*(volatile uint8*)0xffffd8)
-#define IOPORTA_DATA_REG_ADDR		(*(volatile uint8*)0xffffd9)
-#define IOPORTB_DATA_REG_ADDR		(*(volatile uint8*)0xffffda)
+#define DATA_REGADDR		((struct data_regs *)0xffffd0)
 
-//static volatile uint8* p_data_dir_regs = IOPORT_DATADIR_REG_ADDR;
+struct data_dir_regs {
+	volatile uint8 regs[IOPORT_NUM];
+};
 
-//static volatile uint8* p_data_regs = IOPORT_DATA_REG_ADDR;
+static struct data_dir_regs *l_ddr = DATADIR_REGADDR;
+
+struct data_regs {
+	volatile uint8 regs[IOPORT_NUM];
+};
+
+static struct data_regs *l_dr = DATA_REGADDR;
 
 /* デバイス初期化 */
 int ioport_init()
 {
-	// TODO ソフトウェアスタンバイ出力ポートイネーブルの操作を別の場所に移動する
-	// ソフトウェアスタンバイ出力ポートイネーブルをONにする(IOポートのアドレスバス出力を有効にするフラグがこれなんじゃないか？)
-	//SYSCR |= 1 << 1;
-
 	int port_no = 1;
 
 	for (port_no = 1; port_no <= IOPORT_NUM; port_no++)
@@ -51,44 +35,22 @@ int ioport_init()
 	return 0;
 }
 
+// TODO データディレクション設定、ビットをON/OFFする関数を作る
 /* データディレクション設定 */
 int ioport_set_data_direction(int port_no, uint8 data_dir)
 {
-//	int port_idx = port_no - 1;
-//	volatile uint8 *data_dir_reg = &(p_data_dir_regs[port_idx]);
-//	data_dir_reg = data_dir;
-	IOPORT1_DATADIR_REG_ADDR = data_dir;
-	IOPORT2_DATADIR_REG_ADDR = data_dir;
-	IOPORT3_DATADIR_REG_ADDR = data_dir;
-	IOPORT4_DATADIR_REG_ADDR = data_dir;
-	IOPORT5_DATADIR_REG_ADDR = data_dir;
-	IOPORT6_DATADIR_REG_ADDR = data_dir;
-	IOPORT8_DATADIR_REG_ADDR = data_dir;
-	IOPORT9_DATADIR_REG_ADDR = data_dir;
-	IOPORTA_DATADIR_REG_ADDR = data_dir;
-	IOPORTB_DATADIR_REG_ADDR = data_dir;
+	int port_idx = port_no - 1;
+	l_ddr->regs[port_idx] = data_dir;
 
 	return 0;
 }
 
+// TODO データ設定、ビットをON/OFFする関数を作る
 /* データ設定 */
 int ioport_set_data(int port_no, uint8 data)
 {
-//	int port_idx = port_no - 1;
-//	volatile uint8 *data_reg = &(p_data_regs[port_idx]);
-//	data_reg = data;
-	IOPORT1_DATA_REG_ADDR = data;
-	IOPORT2_DATA_REG_ADDR = data;
-	IOPORT3_DATA_REG_ADDR = data;
-	IOPORT4_DATA_REG_ADDR = data;
-	IOPORT5_DATA_REG_ADDR = data;
-	IOPORT6_DATA_REG_ADDR = data;
-	IOPORT7_DATA_REG_ADDR = data;
-	IOPORT8_DATA_REG_ADDR = data;
-	IOPORT9_DATA_REG_ADDR = data;
-	IOPORTA_DATA_REG_ADDR = data;
-	IOPORTB_DATA_REG_ADDR = data;
+	int port_idx = port_no - 1;
+	l_dr->regs[port_idx] = data;
 
-	// TODO あとで直す
-	return IOPORT1_DATA_REG_ADDR;
+	return 0;
 }
