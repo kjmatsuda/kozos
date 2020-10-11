fe7b40650800451e5b1b3ce5eee01df548d0d4fd
Koji Matsuda <kjmatsuda1985@gmail.com>
2020-10-11 17:35:32 +0900

    タイマ関連処理の実装

---
+++ work/os/timer.c
@@ -0,0 +1,71 @@
+#include "defines.h"
+#include "timer.h"
+
+#define TIMER_NUM 3
+
+#define TIMER_COMMON ((volatile struct timer_common *)0xffff60)
+#define TIMER0 ((volatile struct timer *)0xffff68)
+#define TIMER1 ((volatile struct timer *)0xffff70)
+#define TIMER2 ((volatile struct timer *)0xffff78)
+
+struct timer_common {
+	volatile uint8 tstr;		// タイマスタートレジスタ
+	volatile uint8 tsnc;		// タイマシンクロレジスタ
+	volatile uint8 tmdr;		// タイマモードレジスタ
+	volatile uint8 tolr;		// タイマアウトプットレベルセットレジスタ
+	volatile uint8 tisra;		// タイマインタラプトステータスレジスタA
+	volatile uint8 tisrb;		// タイマインタラプトステータスレジスタB
+	volatile uint8 tisrc;		// タイマインタラプトステータスレジスタC
+};
+
+struct timer {
+	volatile uint8 tcr;				// タイマコントロールレジスタ
+	volatile uint8 tior;			// タイマI/Oコントロールレジスタ
+	volatile uint8 tcnt_high;		// タイマカウンタ(High)
+	volatile uint8 tcnt_low;		// タイマカウンタ(Low)
+	volatile uint8 gra_high;		// ジェネラルレジスタA(High)
+	volatile uint8 gra_low;			// ジェネラルレジスタA(Low)
+	volatile uint8 grb_high;		// ジェネラルレジスタB(High)
+	volatile uint8 grb_low;			// ジェネラルレジスタB(Low)
+};
+
+static struct {
+	volatile struct timer *p_timer;
+} l_timer_regs[TIMER_NUM] = {
+	{ TIMER0 },
+	{ TIMER1 },
+	{ TIMER2 },
+};
+
+static struct timer_common *l_timer_common_reg = TIMER_COMMON;
+
+// TODO [タイマー]16TCNT と GRA が一致したら IMFAフラグが1にセットされるらしいが、クリアしなくていいのか？
+// TODO [タイマー]タイマー割込みハンドラの実装
+void timer_init(int index)
+{
+	// 割込み周期を10msecにしたい。システムクロックφは25MHz
+	// 分周比 1 / 4 、GRを62500 にしたら10msec周期で割込みがかかると思われる
+	volatile struct timer *p_timer = l_timer_regs[index].p_timer;
+
+	//// タイマコントロールレジスタ (16TCR)
+	// 「GRA の コンペアマッチ／インプットキャプチャで16TCNT をクリア」にする
+	p_timer->tcr = ~(1<<6) | (1<<5);
+
+	// クロック選択(φ/4)
+	p_timer->tcr = ~(1<<2) | (1<<1) | ~(1<<0);
+
+	//// ジェネラルレジスタGRAを設定
+	// 62500 (0xF424)
+	p_timer->gra_high = 0xF4;
+	p_timer->gra_low = 0x24;
+}
+
+void timer_start(int index)
+{
+	l_timer_common_reg->tstr |= index;
+}
+
+void timer_stop(int index)
+{
+	l_timer_common_reg->tstr &= ~index;
+}
---
+++ work/os/timer.h
@@ -0,0 +1,10 @@
+#ifndef _TIMER_H_INCLUDED_
+#define _TIMER_H_INCLUDED_
+
+void timer_init(int index);
+
+void timer_start(int index);
+
+void timer_stop(int index);
+
+#endif
